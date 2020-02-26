8afe46d85b7b56e8e8bf61941318726374abee4a
Pratham kumar <prathamsolera@gmail.com>
2020-02-26 23:15:00 +0530

    Initial commit with Only UI

---
+++ app.js
@@ -0,0 +1,33 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const nexmo = require("nexmo");
+const socketIO = require("socket.io");
+
+// Init app
+const app = express();
+
+app.use(express.static(__dirname + "public"));
+
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//************* Code **************
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+
+
+// Start Server
+const server = app.listen(3000, function(req, res){
+  console.log("Server started at port 3000");
+});
